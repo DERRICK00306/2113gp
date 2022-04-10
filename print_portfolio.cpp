5c96ea639645dc53a82f845d36c68abc5ab84548
Derrick <86137299+DERRICK00306@users.noreply.github.com>
2022-04-10 23:12:45 +0800

    Add files via upload
    Add function print_portfolio.cpp and print_portfolio.h

---
+++ print_portfolio.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include "print_portfolio.h"
+using namespace std;
+
+void print_portfolio(Asset a[8]) {
+    int i;
+    cout << "The following table shows your portfolio:" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------" << endl; //50 - in intotal
+    cout << "| " << setw(10) << "Name" << " | " << setw(13) <<
+        "Current_price" << " | " << setw(13) << "Average_price" << " | " << setw(7)
+        << "Volumn" << " | " << setw(13) << "Total_earning" << " | " << setw(11) <<
+        "Return_rate" << " | " << setw(12) << "Catogory" << " |" << endl;
+    for (i = 0; i < 8; i++) {
+        double earning; int r;
+        earning = (a[i].current_price - a[i].average_price) * a[i].holding_volume;
+        if (a[i].average_price == 0)r = 0;
+        else r = ((a[i].current_price - a[i].average_price) / a[i].average_price)*100;
+        cout << "| " << setw(10) << a[i].asset_name << " | " << setw(13) <<
+            a[i].current_price << " | " << setw(13) << a[i].average_price << " | " << setw(7)
+            << a[i].holding_volume << " | " << setw(13) << earning << " | " << setw(11) <<
+            to_string( r) + "%" << " | " << setw(12) << a[i].catogory << " |" << endl;
+    }
+    cout << "-----------------------------------------------------------------------------------------------------" << endl;
+}
---
+++ print_portfolio.h
@@ -0,0 +1,5 @@
+#ifndef _PRINT_PORTFOLIO_H
+#define _PRINT_PORTFOLIO_H
+void print_portfolio(Asset a[8]);
+
+#endif
