dc7f7039cb876cf40a18ee70c702b9d2683dbcbf
Derrick <86137299+DERRICK00306@users.noreply.github.com>
2022-04-15 19:00:43 +0800

    Add files via upload

---
+++ transaction.h
@@ -0,0 +1,11 @@
+#ifndef _TRANSACTION_H
+#define _TRANSACTION_H
+
+void grow_transaction(Transaction* &t, int& t_size, int grow);
+
+void add_transaction(Transaction*& t, double price, int volume, string round, string name,
+                      string status, int &number_t, int t_size);
+
+void transaction_review(Transaction*& t, int number_t);
+
+#endif
