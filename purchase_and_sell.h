e85ef8724e993a266c03803242682ae101b0c3f5
Derrick <86137299+DERRICK00306@users.noreply.github.com>
2022-04-30 11:26:35 +0800

    Update purchase_and_sell.h

--- purchase_and_sell.h
+++ purchase_and_sell.h
@@ -1,5 +1,6 @@
 #ifndef _PURCHASE_SELL_H
 #define _PURCHASE_SELL_H
 
-void purchase_or_sell(Asset a[8], string action, string name, int amount, string time);
+void purchase_or_sell(Asset a[8], string action, string name, int amount, string time, Transaction * &t, int &number_t, int &t_size);
+
 #endif
